34c730f2a3310e10ea75c9137586f34eb5decb30
PawPassV.1 <apawcalypsellc@gmail.com>
2026-08-03 10:33:31 -0600

    Fix Android startup navigation crash

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,5 @@
-import { useEffect } from "react";
-import { View, ActivityIndicator } from "react-native";
-import { useRouter } from "expo-router";
+import { Redirect } from "expo-router";
 
 export default function Index() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.replace("/(tabs)/home");
-  }, [router]);
-
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <ActivityIndicator size="large" />
-    </View>
-  );
+  return <Redirect href="/(tabs)/home" />;
 }
